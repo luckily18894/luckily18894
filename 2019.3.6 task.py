234ca327499072a56370e227ac5bcf4a1013d4e1
luckily18894 <185686792@qq.com>
2019-03-06 03:17:39 +0800

    task-5

--- 2019.3.6 task.py
+++ 2019.3.6 task.py
@@ -1,27 +1,11 @@
 
-department1='Security'
-department2='Python'
-manager1='cq_bomb'
-manager2='qinke'
-CF_SEC=456789.12456
-CF_Python=1234.3456
+s1='Port-channel1.189     192.168.189.254  YES   CONFIG  up'
+import re
+s2=re.match('\s*(.+)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(\w+)\s+(\w+)\s+(\w+)\s*', s1).groups()
 
-# 老  中文标点
-line1='Department1 name：%-10s Manager：%-9s COURSE FEES：%-10.2f The End！'%(department1,manager1,CF_SEC)
-line2='Department2 name：%-10s Manager：%-9s COURSE FEES：%-10.2f The End！'%(department2,manager2,CF_Python)
-
-# 新  英文标点
-line3='Department1 name:{0:<10s} Manager:{1:<9s} COURSE FEES:{2:<10.2f} The End!'.format(department1,manager1,CF_SEC)
-line4='Department2 name:{0:<10s} Manager:{1:<9s} COURSE FEES:{2:<10.2f} The End!'.format(department2,manager2,CF_Python)
-
-length1=len(line1)
-print('='*length1)
-print(line1)
-print(line2)
-print('='*length1)
-length2=len(line3)
-print('='*length2)
-print(line3)
-print(line4)
-print('='*length2)
+print('-'*50)
+print(s2)
+print('{0:<5s} : {1:<}'.format('接口',s2[0]))
+print('{0:<5s} : {1:<}'.format('IP地址',s2[1]))
+print('{0:<5s} : {1:<}'.format('状态',s2[4]))
 
