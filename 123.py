33fc2ac089aba7d33a1d3945c056edba1623d3af
luckily18894 <185686792@qq.com>
2019-03-05 23:34:34 +0800

    laptop test

---
+++ 123.py
@@ -0,0 +1,6 @@
+import random
+s1 = random.randint(1,255)
+s2 = random.randint(1,255)
+s3 = random.randint(1,255)
+s4 = random.randint(1,255)
+print(str(s1) + '.' + str(s2) + '.' + str(s3) + '.' + str(s4))
