138b7b237e17e89b784f2dfda948d6fb950b0baf
luckily18894 <185686792@qq.com>
2019-03-06 02:29:16 +0800

    task-4

--- 2019.3.6 task.py
+++ 2019.3.6 task.py
@@ -1,15 +1,27 @@
 
-# solution 1
-word='luckily'
-word1=word[0]
-word2= word[1:]
-print(str(word2)+'-'+str(word1)+'y')
+department1='Security'
+department2='Python'
+manager1='cq_bomb'
+manager2='qinke'
+CF_SEC=456789.12456
+CF_Python=1234.3456
 
-# solution 2
-word='luckily'
-a=str(word)+'-'
-b=a[0]
-c=a[1:]
-d=str(c)+str(b)
-print(d+'y')
+# 老  中文标点
+line1='Department1 name：%-10s Manager：%-9s COURSE FEES：%-10.2f The End！'%(department1,manager1,CF_SEC)
+line2='Department2 name：%-10s Manager：%-9s COURSE FEES：%-10.2f The End！'%(department2,manager2,CF_Python)
+
+# 新  英文标点
+line3='Department1 name:{0:<10s} Manager:{1:<9s} COURSE FEES:{2:<10.2f} The End!'.format(department1,manager1,CF_SEC)
+line4='Department2 name:{0:<10s} Manager:{1:<9s} COURSE FEES:{2:<10.2f} The End!'.format(department2,manager2,CF_Python)
+
+length1=len(line1)
+print('='*length1)
+print(line1)
+print(line2)
+print('='*length1)
+length2=len(line3)
+print('='*length2)
+print(line3)
+print(line4)
+print('='*length2)
 
