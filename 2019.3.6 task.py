c25da82b4a9d8390da24f61ff1b4315d02b36e33
luckily18894 <185686792@qq.com>
2019-03-06 01:34:02 +0800

    task-3

---
+++ 2019.3.6 task.py
@@ -0,0 +1,15 @@
+
+# solution 1
+word='luckily'
+word1=word[0]
+word2= word[1:]
+print(str(word2)+'-'+str(word1)+'y')
+
+# solution 2
+word='luckily'
+a=str(word)+'-'
+b=a[0]
+c=a[1:]
+d=str(c)+str(b)
+print(d+'y')
+
--- 3.6-1.py
+++
@@ -1,5 +0,0 @@
-
-word = 'scallywag'
-sub_word =  word[2:6]
-print(str(sub_word))
-
