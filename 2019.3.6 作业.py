87054991ce253b27bb8183b5fb712b8fcbff3ecf
luckily18894 <185686792@qq.com>
2019-03-07 02:28:51 +0800

    task-4

--- 2019.3.6 作业.py
+++ 2019.3.6 作业.py
@@ -1,10 +1,29 @@
 
 import re
 
-ifcfg = 'eno33554944: flags=4163 mtu 1500\n inet 202.100.1.138 netmask 255.255.255.0 broadcast 202.100.1.255\n inet6 fe80::20c:29ff:fe8d:5cb6 prefixlen 64 scopeid 0x20\n ether 00:0c:29:8d:5c:b6 txqueuelen 1000 (Ethernet)\n RX packets 0 bytes 0 (0.0 B)\n RX errors 0 dropped 0 overruns 0 frame 0\n TX packets 33 bytes 4290 (4.1 KiB)\n TX errors 0 dropped 0 overruns 0 carrier 0 collisions 0\n\n'
-ifcfg1 = re.findall('\w\w:\w\w:\w\w:\w\w:\w\w:\w\w',ifcfg)
-ifcfg2 = re.findall('fe80::\w+:\w{4}:\w{4}:\w{4}',ifcfg)
+s = """TCP Student 192.168.189.167:32806 Teacher 137.78.5.128:65247, idle 0:00:00, bytes 74, flags UIO
+TCP Student 192.168.189.167:80 Teacher 137.78.5.128:65233, idle 0:00:03, bytes 334516, flags UIO"""
 
-print('{0:>7} : {1:>}'.format('MAC地址',ifcfg1[0]))
-print('{0:>7} : {1:>}'.format('ipv6地址',ifcfg2[0]))
+l1 = s.split('\n')
+dc1 = {}
+
+for x in l1:
+    r = re.match(
+        '.+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d+)\s*Teacher\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d+).+bytes\s*(\d+).+flags\s*(\w*)\s*',
+        x).groups()
+    key1 = r[0],r[1],r[2],r[3]
+    value1 = r[4],r[5]
+    dc1[key1] = value1
+
+print('\n打印字典\n')
+print(dc1)
+print('\n格式化输出\n')
+print('='*100)
+
+for key1,value1 in dc1.items():
+    print('{0:>8} : {1:<15}|{2:>9} : {3:<6}|{4:>7} : {5:<18}|{6:>9} : {7:<12}|'.format('src', key1[0], 'src_p', key1[1],
+                                                                                       'dst', key1[2], 'dst_p',
+                                                                                       key1[3]))
+    print('{0:>8} : {1:<15}|{2:>9} : {3:<6}|{4:^28}|{4:^24}|'.format('bytes', value1[0], 'flags', value1[1],' '))
+    print('=' * 100)
 
