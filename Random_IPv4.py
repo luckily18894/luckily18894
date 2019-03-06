1da3208aa188aced8b875f967fa2860562e89356
luckily18894 <185686792@qq.com>
2019-03-06 09:55:48 +0800

    2019.3.4 task-2

---
+++ Random_IPv4.py
@@ -0,0 +1,7 @@
+import random
+section1 = random.randint(1,255)
+section2 = random.randint(1,255)
+section3 = random.randint(1,255)
+section4 = random.randint(1,255)
+print (str(section1) + '.' + str(section2) + '.' + str(section3) + '.' + str(section4))
+
