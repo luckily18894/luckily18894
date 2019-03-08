d3004a37362d645e40b2a7bb4f98c76f7487fcf7
luckily18894 <185686792@qq.com>
2019-03-08 22:33:51 +0800

    task-1

---
+++ 2019.3.8 作业/接口排序.py
@@ -0,0 +1,7 @@
+
+port_list = ['eth 1/101/1/42','eth 1/101/1/26','eth 1/101/1/23','eth 1/101/1/7','eth 1/101/2/46','eth 1/101/1/34','eth 1/101/1/18','eth 1/101/1/13','eth 1/101/1/32','eth 1/101/1/25','eth 1/101/1/45','eth 1/101/2/8']
+
+l2 = sorted(port_list,key=lambda x:x[-2:])
+
+print(sorted(l2,key=lambda x:x[10]))
+
